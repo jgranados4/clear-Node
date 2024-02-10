ba898dd7784db7dd68b4d980be25c4951c649e02
jgranados4 <jgranados@est.ups.edu.ec>
2024-02-10 10:27:47 -0500

    Agregada función de inicio de sesión en AuthDatasourceImpl

--- src/infrastructure/datasource/auth.datasource.impl.ts
+++ src/infrastructure/datasource/auth.datasource.impl.ts
@@ -4,6 +4,7 @@ import { UserModel } from "../../data/mongoDB";
 import {
   AuthDatasource,
   CustomError,
+  LoginUserDto,
   RegisterUserDto,
   UserEntity,
 } from "../../domain";
@@ -17,13 +18,30 @@ export class AuthDatasourceImpl implements AuthDatasource {
     private readonly hashPassword: HashFunction = BcryptAdapter.hash,
     private readonly comparePassword: CompareFunction = BcryptAdapter.compare
   ) {}
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await UserModel.findOne({ email });
+      if (!user) throw CustomError.badRequest("User does not exists - email");
+
+      const isMatching = this.comparePassword(password, user.password);
+      if (!isMatching) throw CustomError.badRequest("Password is not valid");
+
+      return UserMapper.userEntityFromObject(user);
+    } catch (error) {
+      console.log(error);
+      throw CustomError.internalServer();
+    }
+  }
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
     const { name, email, password } = registerUserDto;
 
     try {
       //
       const exists = await UserModel.findOne({ email });
-      if (exists) throw CustomError.badRequest("Las credenciales no son correctas");
+      if (exists)
+        throw CustomError.badRequest("Las credenciales no son correctas");
       //
       const user = await UserModel.create({
         name: name,
