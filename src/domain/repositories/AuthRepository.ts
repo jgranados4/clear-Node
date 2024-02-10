c58e2344414eb6a375c6788aab8b6db335662df6
jgranados4 <jgranados@est.ups.edu.ec>
2024-02-10 10:27:28 -0500

    Agregada función de inicio de sesión en AuthRepository.ts

--- src/domain/repositories/AuthRepository.ts
+++ src/domain/repositories/AuthRepository.ts
@@ -1,6 +1,8 @@
+import { LoginUserDto } from "../dtos/Auth/login-user.dto";
 import { RegisterUserDto } from "../dtos/Auth/register-user.dto";
 import { UserEntity } from "../entities/user.entity";
 
 export abstract class AuthRepository {
+  abstract login(loginUserDto: LoginUserDto): Promise<UserEntity>;
   abstract register(registerUserDto: RegisterUserDto): Promise<UserEntity>;
 }
